10eee5300bb1466ff76aacea1d1527ff5878b1ed
Vasprato <contacto@vasprato.com>
2026-01-17 21:47:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
-// Servir estáticos
 app.use(express.static(path.join(__dirname, "public")));
 
-// Home
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = Number(process.env.PORT || 3000);
 app.listen(PORT, "0.0.0.0", () => console.log("Running on", PORT));
-
