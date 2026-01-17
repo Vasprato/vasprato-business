fb2b0f4813c43f65d245c23f9c2657436c0e065c
Vasprato <contacto@vasprato.com>
2026-01-17 21:40:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const path = require("path");
 
 const app = express();
 
-// (opcional) servir estáticos si tienes /public
+// Servir estáticos
 app.use(express.static(path.join(__dirname, "public")));
 
+// Home
 app.get("/", (req, res) => {
-  res.send("VASPRATO Business Online ✅");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const PORT = Number(process.env.PORT || 3000);
-const HOST = "0.0.0.0";
-
-app.listen(PORT, HOST, () => {
-  console.log(`Server running on http://${HOST}:${PORT}`);
-});
-
+app.listen(PORT, "0.0.0.0", () => console.log("Running on", PORT));
 
